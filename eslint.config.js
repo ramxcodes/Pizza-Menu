4096663f9692e8f346c56a65111948c3820e3406
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-04 16:41:24 +0530

    Tweaks & Updates 🔧

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-
+// Ram
 export default [
   { ignores: ['dist'] },
   {
